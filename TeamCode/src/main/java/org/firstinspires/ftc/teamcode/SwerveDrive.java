8f3ac7ef7dec341ecc79e6c5f1d9c003ebba6923
Scifighters <ftctelmond@gmail.com>
2024-10-28 21:28:11 +0200

    started swervedrive and swervedriveopmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SwerveDrive.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class SwerveDrive {
+    SwerveModule bl,br,fl,fr;
+    public SwerveDrive(HardwareMap hardwareMap){
+        bl = new SwerveModule(hardwareMap.get(DcMotor.class, "bl_motor"),
+                hardwareMap.get(CRServo.class, "bl_servo"),
+                hardwareMap.analogInput.get("bl_encoder"),147.85321100917432);
+        br = new SwerveModule(hardwareMap.get(DcMotor.class, "br_motor"),
+                hardwareMap.get(CRServo.class, "br_servo"),
+                hardwareMap.analogInput.get("br_encoder"),77.94495412844036);
+        fl = new SwerveModule(hardwareMap.get(DcMotor.class, "fl_motor"),
+                hardwareMap.get(CRServo.class, "fl_servo"),
+                hardwareMap.analogInput.get("fl_encoder"),44.697247706422026);
+        fr = new SwerveModule(hardwareMap.get(DcMotor.class, "fr_motor"),
+                hardwareMap.get(CRServo.class, "fr_servo"),
+                hardwareMap.analogInput.get("fr_encoder"),84);
+
+    }
+
+    public void update(){
+        //update
+    }
+
+}
