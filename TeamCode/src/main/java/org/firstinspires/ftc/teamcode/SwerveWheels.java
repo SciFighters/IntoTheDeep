d12575a76e47519bcf23c00a58d2d9900155a280
Scifighters <ftctelmond@gmail.com>
2024-10-30 20:21:55 +0200

    fixed a wierd bug about wheel angles

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SwerveWheels.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SwerveWheels.java
@@ -3,10 +3,11 @@ package org.firstinspires.ftc.teamcode;
 
 
 public enum SwerveWheels {
+    //372 wide 344 other wide
     FL(Math.atan(372/344)),
-    FR(180 - Math.atan(372/344)),
-    BR(180 + Math.atan(372/344)),
-    BL(360 - Math.toRadians(372/344));
+    FR(Math.PI - Math.atan(372/344)),
+    BR(Math.PI + Math.atan(372/344)),
+    BL(- Math.atan(372/344));
 
     public final double ANGLE;
     private SwerveWheels(double angle){
